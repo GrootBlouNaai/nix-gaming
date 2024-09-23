2dceb84c2b69eff9106a8a19e6d78d126a5b0e85
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-09-23 18:28:07 +0200

    Explain

--- pkgs/wine/default.nix
+++ pkgs/wine/default.nix
@@ -14,6 +14,10 @@
   supportFlags,
   stdenv_32bit,
 }: let
+  # Path filtering function
+  # This function filters the nixpkgs repository to include only the Wine-related files.
+  # It ensures that only the necessary parts of the repository are included in the build,
+  # reducing the overall size and complexity of the build environment.
   nixpkgs-wine = builtins.path {
     path = inputs.nixpkgs;
     name = "source";
@@ -25,6 +29,9 @@
     );
   };
 
+  # Default configuration for Wine builds
+  # This section defines the default settings and dependencies for building Wine.
+  # It includes support flags, build scripts, configuration flags, and other necessary components.
   defaults = let
     sources = (import "${inputs.nixpkgs}/pkgs/applications/emulators/wine/sources.nix" {inherit pkgs;}).unstable;
   in {
@@ -41,8 +48,14 @@
     wineRelease = "unstable";
   };
 
+  # Function to generate package names
+  # This function generates the package name based on the build type (e.g., "full" or default).
+  # It appends "-full" to the package name if the build type is "full".
   pnameGen = n: n + lib.optionalString (build == "full") "-full";
 in {
+  # Wine-GE package definition
+  # This section defines the Wine-GE package, which is a custom build of Wine optimized for gaming.
+  # It uses the base configuration with additional overrides for specific attributes.
   wine-ge =
     (callPackage "${nixpkgs-wine}/pkgs/applications/emulators/wine/base.nix" (defaults
       // {
@@ -54,6 +67,9 @@ in {
       meta = old.meta // {passthru.updateScript = ./update-wine-ge.sh;};
     });
 
+  # Wine-TKG package definition
+  # This section defines the Wine-TKG package, which is another custom build of Wine optimized for gaming.
+  # It uses the base configuration with additional overrides for specific attributes.
   wine-tkg = callPackage "${nixpkgs-wine}/pkgs/applications/emulators/wine/base.nix" (lib.recursiveUpdate defaults
     rec {
       pname = pnameGen "wine-tkg";
@@ -61,6 +77,9 @@ in {
       src = pins.wine-tkg;
     });
 
+  # Wine-OSU package definition
+  # This section defines the Wine-OSU package, which is a custom build of Wine optimized for the osu! game.
+  # It includes additional patches and dependencies specific to the osu! game.
   wine-osu = let
     pname = pnameGen "wine-osu";
     version = "7.0";
